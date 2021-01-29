f09400f30a693bc1ef5154c51d7f0376855aebd8
ggferreira <Gg545800>
2021-01-29 09:45:31 -0300

    Criado atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def comprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Gabriel')
     print(Pessoa.comprimentar(p))
     print(f'    {id(p)}')
     print(p.comprimentar())
+    print(p.nome)
