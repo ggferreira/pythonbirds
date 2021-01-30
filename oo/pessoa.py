08b9d22e4fe72a093f8c83e7b980293f27e064a5
ggferreira <Gg545800>
2021-01-30 11:12:17 -0300

    Criado atributo atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def comprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Gabriel')
-    print(Pessoa.comprimentar(p))
-    print(f'    {id(p)}')
-    print(p.comprimentar())
-    print(p.nome)
+    gabriel = Pessoa(nome='Gabriel')
+    luciano = Pessoa(gabriel, nome='Luciano')
+    print(Pessoa.comprimentar(luciano))
+    print(id(luciano))
+    print(luciano.comprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
